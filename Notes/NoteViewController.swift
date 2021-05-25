14ea22a535756f50aff5457ed79eb16c0391fd75
Frank Hofman <frank-hofman77@live.nl>
2021-05-25 15:15:46 +0200

    still trying to lower keyboard properly

--- Notes/NoteViewController.swift
+++ Notes/NoteViewController.swift
@@ -5,6 +5,18 @@ class NoteViewController: UIViewController {
     
     var note: Note? = nil
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//      source for tap to dismiss keyboard: https://stackoverflow.com/questions/24126678/close-ios-keyboard-by-touching-anywhere-using-swift by Esqarrouth
+        let tap = UITapGestureRecognizer(target: contentTextView, action: #selector(self.dismissKeyboard (_:)))
+        contentTextView.addGestureRecognizer(tap)
+    }
+
+    @objc func dismissKeyboard (_ sender: UITapGestureRecognizer) {
+        print("tab recognition works")
+        view.endEditing(true)
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
